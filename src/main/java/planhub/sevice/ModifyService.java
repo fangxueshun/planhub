8fabb3be88f72ae14801cfe557895b2d1bbf0444
Marinefish <fangxueshun@126.com>
2016-07-07 23:04:20 +0800

    add modify userName function,fix some bugs

---
+++ src/main/java/planhub/sevice/ModifyService.java
@@ -0,0 +1,7 @@
+package planhub.sevice;
+
+/**
+ * Created by Administrator on 2016/7/7.
+ */
+public class ModifyService {
+}
--- src/main/java/planhub/web/ModifyUserValueCtr.java
+++ src/main/java/planhub/web/ModifyUserValueCtr.java
@@ -3,6 +3,7 @@ package planhub.web;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,32 +11,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import planhub.domain.User;
 import planhub.sevice.LoginService;
+import planhub.sevice.ModifyService;
 
 import javax.validation.Valid;
 
 /**
  * Created by little_sheep on 2016/7/6.
  */
-public class ModifyUserValue {
+@Controller
+public class ModifyUserValueCtr {
     @Autowired
-    private LoginService loginService;
+    private ModifyService modifyService;
 
     //特定字段不进行反序列化
     Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
-//    @RequestMapping(value = "/v01/user/modify",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
+    @RequestMapping(value = "/v01/user/modify",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
 
-//    @ResponseBody
-//    public String login(@RequestBody @Valid Map<String,Object>, BindingResult result){
-//        if (result.hasErrors())
-//            return gson.toJson(result.getAllErrors());
-//        else{
-//            if (loginService.login(user)) {
-//
-//                return gson.toJson(user);
-//            }
-//            else{
-//                return "登录失败";
-//            }
-//        }
-//    }
+    @ResponseBody
+    public String login(@RequestBody @Valid User user, BindingResult result){
+        if (result.hasErrors())
+            return gson.toJson(result.getAllErrors());
+        else{
+            return gson.toJson(modifyService.modifyService(user));
+        }
+    }
 }
